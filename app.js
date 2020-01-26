dae93d5d53bfd7c5439ed5f4482ca9bd5981ea84
mac <mac@macs-MacBook-Air.local>
2020-01-26 17:59:59 +0200

    initial

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/api/position', positionRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/dist/client'));
 
-    app.get('*', (req, res) => {
+    app.all('*', (req, res) => {
         res.sendFile(
             path.resolve(
                 __dirname, 'client','dist', 'client', 'index.html'
